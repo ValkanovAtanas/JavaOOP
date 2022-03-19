af24b97cc9f72172d671df9abd586e2608b6adb8
Atanas (home PC) <valkanovatanas@gmail.com>
2022-03-19 18:14:06 +0200

    Single Inheritance solution and creating Multiple Inheritance package and Main class in it

---
+++ src/inheritance/multiInheritance/Main.java
@@ -0,0 +1,4 @@
+package inheritance.multiInheritance;
+
+public class Main {
+}
--- src/inheritance/singleInheritance/Main.java
+++ src/inheritance/singleInheritance/Main.java
@@ -1,4 +1,9 @@
 package inheritance.singleInheritance;
 
 public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
 }
