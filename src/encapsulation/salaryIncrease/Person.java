1aa793284687440a3faa046cb64063bb481cd676
Atanas <valkanovatanas@gmail.com>
2022-03-22 12:44:18 +0200

    Salary Increase solution

---
+++ src/encapsulation/salaryIncrease/Person.java
@@ -0,0 +1,62 @@
+package encapsulation.salaryIncrease;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %s leva",
+                this.firstName,
+                this.lastName,
+                this.salary);
+    }
+
+    public void increaseSalary(double bonus) {
+        if(this.age < 30)
+            bonus /= 2;
+        double bonusAmount = this.salary * bonus / 100;
+        this.salary += bonusAmount;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
