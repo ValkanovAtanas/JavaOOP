c033e37dd4f8c205abfafc04db87ee99bbdf17c4
Atanas <valkanovatanas@gmail.com>
2022-03-22 12:29:57 +0200

    Sort by Name and Age solution

---
+++ src/inheritance/restaurant/Main.java
@@ -0,0 +1,11 @@
+package inheritance.restaurant;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        Salmon salmon = new Salmon("S", BigDecimal.valueOf(5));
+
+        System.out.println();
+    }
+}
